daab4a983b4e6a205b8427e092d41cb63c63671e
Rick Landon <rlandon@usa.net>
2026-06-18 11:07:03 -0700

    feat: establish Tziphora opening canon slice

---
+++ scripts/canon-bootstap-canon-entries-tziphora.bash
@@ -0,0 +1,8 @@
+cp templates/character-template.adoc canon/characters/character-tziphora-cohen.adoc
+cp templates/artifact-template.adoc canon/artifacts/artifact-book-night.adoc
+cp templates/timeline-template.adoc canon/timeline/timeline-tziphora-discovers-night.adoc
+cp templates/character-template.adoc canon/characters/character-teacher.adoc
+cp templates/timeline-template.adoc canon/timeline/timeline-show-and-tell.adoc
+cp templates/symbol-template.adoc canon/symbols/symbol-red-coat.adoc
+cp templates/mystery-template.adoc canon/mysteries/mystery-girl-in-red-coat.adoc
+cp templates/provenance-template.adoc canon/provenance/prov-0001-tziphora-opening-thread.adoc
